4bea66025fdc9b64f0a419180aaff59693c9a09e
Diego de los Reyes <diegorys@gmail.com>
2018-03-10 18:30:57 +0100

    3 - Panda dataframes example.

--- 3-panda-dataframes/panda-dataframes.py
+++ 3-panda-dataframes/panda-dataframes.py
@@ -1,11 +1,20 @@
 # Python
 import pandas as pd
+import matplotlib
 lightSensor = pd.Series([234, 42, 54, 192, 242, 23])
 temperatureSensor = pd.Series([23, 18, 20, 26, 30, 15])
 humiditySensor = pd.Series([40, 35, 20, 57, 18, 45])
-sensors = pd.DataFrame({
-    'Ligth': lightSensor,
-    'Temperature': temperatureSensor,
-    'Humidity': humiditySensor
+sensorsDataFrame = pd.DataFrame({
+    'ligth': lightSensor,
+    'temperature': temperatureSensor,
+    'humidity': humiditySensor
 })
-print(sensors)
+#sensorsDataFrame.to_csv("sensors.csv", sep=",")
+sensorsDataFrame = pd.read_csv("sensors.csv", sep=",")
+print(sensorsDataFrame)
+desc = sensorsDataFrame.describe()
+print(desc)
+head = sensorsDataFrame.head(2)
+print(head)
+hist = sensorsDataFrame.hist()
+print(hist)
