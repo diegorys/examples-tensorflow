8b8f651be69a6ea8685c8424def59b3f11f23d6c
Diego de los Reyes <diegorys@gmail.com>
2018-04-03 00:06:03 +0200

    Hi Tensorflow modified.

--- hi/hi-tensorflow.py
+++ hi/hi-tensorflow.py
@@ -1,5 +1,12 @@
-# Python
 import tensorflow as tf
-hello = tf.constant('Hello, TensorFlow!')
+
+first = tf.placeholder("float")
+second = tf.placeholder("float")
+
+y_sum = tf.add(first, second)
+y_mul = tf.mul(first, second)
+
 sess = tf.Session()
-print(sess.run(hello))
+
+print sess.run(y_sum, feed_dict={first: 2, second: 3})
+print sess.run(y_mul, feed_dict={first: 2, second: 3})
