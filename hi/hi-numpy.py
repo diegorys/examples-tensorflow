767a7347e949a8ee8614937986b84f8761c5b5ee
Diego de los Reyes <diegorys@gmail.com>
2019-11-11 18:52:26 +0100

    Hi Numpy

--- hi/hi-numpy.py
+++ hi/hi-numpy.py
@@ -2,5 +2,11 @@
 import numpy as np
 
 print("Hi NumPy")
-data = np.array([1, 2, 3])
-print(data)
+v1 = np.array([1, 2, 3])
+print("V1", v1)
+v2 = np.array([4, 5, 6])
+print("V2", v2)
+v3 = v1 + v2
+print("V1 + V2", v3)
+v4 = v1 * v2
+print("V1 * V2", v4)
