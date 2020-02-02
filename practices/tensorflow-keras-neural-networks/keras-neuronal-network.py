d51a4f4bf45f2323ea4a0527f8096278e1997487
Diego de los Reyes <diegorys@gmail.com>
2020-02-02 12:56:38 +0100

    First neuronal network.

--- practices/tensorflow-keras-neural-networks/keras-neuronal-network.py
+++ practices/tensorflow-keras-neural-networks/keras-neuronal-network.py
@@ -8,5 +8,5 @@ ys = np.array([-10.0, -4.0, 8.0, -1.0, 14.0, 11.0, 5.0, -13.0], dtype=float)
 model = keras.Sequential([keras.layers.Dense(units=1, input_shape=[1])])
 model.compile(optimizer='sgd', loss='mean_squared_error')
 
-model.fit(xs, ys, epochs=10000)
+model.fit(xs, ys, epochs=300)
 print(model.predict([8.0]))
