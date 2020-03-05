2d5f00be97c3b8c1abcadb5b819bafcce0d4ad74
Diego de los Reyes <diegorys@gmail.com>
2020-03-05 20:31:34 +0100

    Hi Pandas.

--- hi/hi-pandas.py
+++ hi/hi-pandas.py
@@ -1,5 +1,21 @@
 # Python
 import pandas as pd
+import numpy as np
+
 print("--------------------------------------")
 print("Hi Pandas! Version: " + pd.__version__)
 print("--------------------------------------")
+
+raw_data = {'titulo': ['Clean Code', 'Head First Design Patterns', 'Artificial Intelligence with Python', 'Electrónica para Makers'], 
+        'autor': ['Robert C. Martin', 'Eric Freeman', 'Prateek Joshi', 'Paolo Aliverti'], 
+        'paginas': [464, 694, 446, 376],
+        'precio': [35.40, 41.00, 45.74, 23.70],
+        'puntuacion': [4.6, 4.5, 3.5, 4.4]
+}
+df = pd.DataFrame(raw_data, columns = ['titulo', 'autor', 'paginas', 'precio', 'puntuacion'])
+print(df)
+
+print("Precio medio:", df["precio"].mean())
+print("Puntuación media:", df["puntuacion"].mean())
+print("Número de páginas (máximo):", df["paginas"].max())
+print("Número de páginas (mínimo):", df["paginas"].min())
